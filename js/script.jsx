5bf82d9a9040ed49b22aec74f69cc98599e015b9
aurelianonava <sersinfin@gmail.com>
2015-09-17 20:08:39 -0700

    Added incremetation to button component, and moved Reveal presentation file to root.

--- js/script.jsx
+++ js/script.jsx
@@ -1,6 +1,13 @@
 var Button = React.createClass({
+    getInitialState: function() {
+        return {counter: 0}
+    },
+    handleClick: function() {
+        this.setState({ counter: this.state.counter+1});
+        },
     render: function() {
-        return (<button>Go!</button>
+        return (
+            <button onClick={this.handleClick}>{this.state.counter}</button>
         )
     }
 });
